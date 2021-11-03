b47edf760f128b5ed54b06d9d80817b4f6dc4a4b
mhd1233 <mohamad.naserhagal1@gmail.com>
2021-11-03 23:40:32 +0500

    ff

---
+++ js/javascrebt.js
@@ -0,0 +1,16 @@
+
+function sayhellow(){
+  var name= document.getElementById("name").value ;
+ alert("hallow kira web sit nice to meet you *-* ");
+  if(name ==="jsome"||name==="J"||name ==="j"){
+    var massage= "<h2>good by  "+ name + " </h2> " ;
+    document
+    .getElementById("contect")
+    .innerHTML =massage ;
+  }else{
+    var massage= "<h2>hellow  "+ name + " </h2> " ;
+    document
+    .getElementById("contect")
+    .innerHTML =massage ;
+  }}
+ 

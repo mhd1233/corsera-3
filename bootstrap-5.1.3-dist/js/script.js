2a87989ec8cac7896c97b3447e47eb87dfd318b5
mhd1233 <mohamad.naserhagal1@gmail.com>
2021-11-15 13:55:21 +0500

    hay me kira

--- bootstrap-5.1.3-dist/js/script.js
+++ bootstrap-5.1.3-dist/js/script.js
@@ -3,13 +3,11 @@
 var homeHtmlUrl="snippet/home-sinpping.html";
 
 
-
 var insertHtml = function (selector, html1) {
     var targetElem = document.querySelector(selector);
     targetElem.innerHTML = html1;
   };
-
-
+  
 
   var showLoading = function (selector) {
     var html1 = "<div class='text-center'>";
@@ -20,10 +18,16 @@ var insertHtml = function (selector, html1) {
   document.addEventListener("DOMContentLoaded", function (event) {
 
     showLoading("#main-content");
+    
     $("#main-content").load(homeHtmlUrl);
 
-  }
+  });
 
+   $("#manue").click(function(){
+    showLoading("#main-content");
+    
+    $("#main-content").load($(this).data("page"));
      
-     ); 
+   });
+      
     })(window);
